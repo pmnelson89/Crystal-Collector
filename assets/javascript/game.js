5dcb12586de7e759caa38e533cd3f8c16b5a8af4
Paul Nelson <pmnelso@gmail.com>
2019-02-27 21:11:45 -0500

    made jquery functional on front end

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,37 +4,53 @@ $(document).ready(function () {
     var wins = 0;
     var losses = 0;
 
-    function gameBegin() {
+    //set initial variable values
 
-        //set initial variable values
-        var targetNum = Math.floor(Math.random() * 101 + 19);
-        var redValue = Math.floor(Math.random() * 12 + 1);
-        var blueValue = Math.floor(Math.random() * 12 + 1);
-        var greenValue = Math.floor(Math.random() * 12 + 1);
-        var yellowValue = Math.floor(Math.random() * 12 + 1);
-        losses = 0;
-        wins = 0;
+    var targetNum = Math.floor(Math.random() * 101 + 19);
+    var redValue = Math.floor(Math.random() * 12 + 1);
+    var blueValue = Math.floor(Math.random() * 12 + 1);
+    var greenValue = Math.floor(Math.random() * 12 + 1);
+    var yellowValue = Math.floor(Math.random() * 12 + 1);
+    totalScore = 0;
+
+    //assign values to buttons
+
+    $("#redCrystal").attr("value", redValue);
+    $("#blueCrystal").attr("value", blueValue);
+    $("#greenCrystal").attr("value", greenValue);
+    $("#yellowCrystal").attr("value", yellowValue);
+
+    //display values in html
+
+    $("#targetText").text(targetNum);
+    $("#scoreText").text(totalScore);
+    
+    console.log("red = " + redValue);
+    console.log("blue = " + blueValue);
+    console.log("green = " + greenValue);
+    console.log("yellow = " + yellowValue);
+
+    //reset button values and scoreboard
+
+    function resetRound() {
+        targetNum = Math.floor(Math.random() * 101 + 19);
+        redValue = Math.floor(Math.random() * 12 + 1);
+        blueValue = Math.floor(Math.random() * 12 + 1);
+        greenValue = Math.floor(Math.random() * 12 + 1);
+        yellowValue = Math.floor(Math.random() * 12 + 1);
         totalScore = 0;
-
-        //assign values to buttons
-
         $("#redCrystal").attr("value", redValue);
         $("#blueCrystal").attr("value", blueValue);
         $("#greenCrystal").attr("value", greenValue);
         $("#yellowCrystal").attr("value", yellowValue);
-
-        //display values in html
-
         $("#targetText").text(targetNum);
-        $("#winText").text(wins);
-        $("#lossText").text(losses);
         $("#scoreText").text(totalScore);
         console.log("red = " + redValue);
         console.log("blue = " + blueValue);
         console.log("green = " + greenValue);
         console.log("yellow = " + yellowValue);
-
     };
+
 
     //grab values when buttons are clicked and add them to total
 
@@ -47,10 +63,21 @@ $(document).ready(function () {
         totalScore += crystalValue;
 
         console.log("total = " + totalScore);
+        $("#scoreText").text(totalScore);
+
+        if (totalScore === targetNum) {
+            wins++;
+            $("#winText").text(wins);
+            resetRound();
+        }
+        else if (totalScore >= targetNum) {
+            losses++;
+            $("#lossText").text(losses);
+            resetRound();
+        }
 
     });
 
-    gameBegin();
 
 });
 
