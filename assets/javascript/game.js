f114dbc5d07ee22ca2c5d25d144c23bff26e560f
Paul Nelson <pmnelso@gmail.com>
2019-02-27 20:13:30 -0500

    fixed jquery issue

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,53 +1,57 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-    function gameBegin() {
-
-//set initial variable values
-    var targetNum = Math.floor(Math.random() * 101 + 19);
-    var redValue = Math.floor(Math.random() * 12 + 1);
-    var blueValue = Math.floor(Math.random() * 12 + 1);
-    var greenValue = Math.floor(Math.random() * 12 + 1);
-    var yellowValue = Math.floor(Math.random() * 12 + 1);
     var totalScore = 0;
     var wins = 0;
     var losses = 0;
 
-//assign values to buttons
+    function gameBegin() {
 
-    $("#redCrystal").attr("value", redValue);
-    $("#blueCrystal").attr("value", blueValue);
-    $("#greenCrystal").attr("value", greenValue);
-    $("#yellowCrystal").attr("value", yellowValue);
+        //set initial variable values
+        var targetNum = Math.floor(Math.random() * 101 + 19);
+        var redValue = Math.floor(Math.random() * 12 + 1);
+        var blueValue = Math.floor(Math.random() * 12 + 1);
+        var greenValue = Math.floor(Math.random() * 12 + 1);
+        var yellowValue = Math.floor(Math.random() * 12 + 1);
+        losses = 0;
+        wins = 0;
+        totalScore = 0;
 
-//display values in html
+        //assign values to buttons
 
-    $("#targetText").text(targetNum);
-    $("#winText").text(wins);
-    $("#lossText").text(losses);
-    $("#scoreText").text(totalScore);
-    console.log("red = " + redValue);
-    console.log("blue = " + blueValue);
-    console.log("green = " + greenValue);
-    console.log("yellow = " + yellowValue);
-    
+        $("#redCrystal").attr("value", redValue);
+        $("#blueCrystal").attr("value", blueValue);
+        $("#greenCrystal").attr("value", greenValue);
+        $("#yellowCrystal").attr("value", yellowValue);
+
+        //display values in html
+
+        $("#targetText").text(targetNum);
+        $("#winText").text(wins);
+        $("#lossText").text(losses);
+        $("#scoreText").text(totalScore);
+        console.log("red = " + redValue);
+        console.log("blue = " + blueValue);
+        console.log("green = " + greenValue);
+        console.log("yellow = " + yellowValue);
+
     };
 
-//grab values when buttons are clicked and add them to total
+    //grab values when buttons are clicked and add them to total
 
-    $(".crystal-image").on("click", function() {
+    $(".crystal-button").on("click", function () {
 
         var crystalValue = ($(this).attr("value"));
         crystalValue = parseInt(crystalValue);
         totalScore = parseInt(totalScore);
 
         totalScore += crystalValue;
-        
+
         console.log("total = " + totalScore);
 
     });
 
     gameBegin();
-   
+
 });
 
 
